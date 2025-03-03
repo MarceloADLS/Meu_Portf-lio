54d3c465f1aabb1e3780fc413de8ffa0044dec94
MarceloADLS <marceloaugustodls@hotmail.com>
2025-03-03 14:32:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-// Navegação entre abas
 const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', function() {
-        // Remover a classe 'active' de todas as abas e seções
+    tab.addEventListener('click', function (e) {
+        e.preventDefault();
+
         tabs.forEach(t => t.classList.remove('active'));
         tabContents.forEach(content => content.classList.remove('active'));
-        
-        // Adicionar a classe 'active' à aba clicada e à seção correspondente
-        tab.classList.add('active');
-        const tabId = tab.getAttribute('data-tab');
-        document.getElementById(tabId).classList.add('active');
-    });
-});
 
-// Ativar a primeira aba por padrão
-tabs[0].classList.add('active');
-tabContents[0].classList.add('active');
+        tab.classList.add('active');
+        const tabId = tab.getAttribute('href').substring(1); // Obtém o ID da aba
+        document.getElementById(tabId
