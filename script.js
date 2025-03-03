569a82d877424f64e099c9868b4e7679cd02debe
MarceloADLS <marceloaugustodls@hotmail.com>
2025-03-03 15:15:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ tabs.forEach(tab => {
     tab.addEventListener('click', function (e) {
         e.preventDefault();
 
+        // Remover a classe 'active' de todas as abas e conteúdos
         tabs.forEach(t => t.classList.remove('active'));
         tabContents.forEach(content => content.classList.remove('active'));
 
+        // Adicionar a classe 'active' à aba clicada
         tab.classList.add('active');
-        const tabId = tab.getAttribute('href').substring(1); // Obtém o ID da aba
-        document.getElementById(tabId).classList.add('active'); // Torna a aba ativa visível
+        
+        // Obter o ID da aba clicada e exibir o conteúdo correspondente
+        const tabId = tab.getAttribute('href').substring(1); // Obtém o ID da aba (sem o '#')
+        const tabContent = document.getElementById(tabId); // Seleciona o conteúdo da aba
+        tabContent.classList.add('active'); // Exibe o conteúdo da aba
     });
 });
