86918c6dd334010f434e036e85dd7580d62cbbe6
MarceloADLS <marceloaugustodls@hotmail.com>
2025-03-04 00:26:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const tabs = document.querySelectorAll('.tab');
-const tabContents = document.querySelectorAll('.tab-content');
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', function (e) {
-        e.preventDefault();
+    tabs.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            e.preventDefault();
+            tabs.forEach(tab => tab.classList.remove('active'));
+            tab.classList.add('active');
 
-        // Remover a classe 'active' de todas as abas e conteúdos
-        tabs.forEach(t => t.classList.remove('active'));
-        tabContents.forEach(content => content.classList.remove('active'));
+            const target = tab.getAttribute('href').slice(1);
 
-        // Adicionar a classe 'active' à aba clicada
-        tab.classList.add('active');
-        
-        // Obter o ID da aba clicada e exibir o conteúdo correspondente
-        const tabId = tab.getAttribute('href').substring(1); // Obtém o ID da aba (sem o '#')
-        const tabContent = document.getElementById(tabId); // Seleciona o conteúdo da aba
-        tabContent.classList.add('active'); // Exibe o conteúdo da aba
+            tabContents.forEach(content => {
+                content.classList.remove('active');
+                if (content.id === target) {
+                    content.classList.add('active');
+                }
+            });
+        });
     });
 });
